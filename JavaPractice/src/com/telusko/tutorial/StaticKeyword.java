be1f507dded5aa80d0f24f67d58153e8cacf66e1
JyotiRSharma <appdeveloperubuntu@gmail.com>
2020-11-13 17:40:24 +0530

    More info on static

--- JavaPractice/src/com/telusko/tutorial/StaticKeyword.java
+++ JavaPractice/src/com/telusko/tutorial/StaticKeyword.java
@@ -5,6 +5,15 @@ class Emp {
 	int salary;
 	static String ceo;
 	
+	static { //Static block gets called only once with the class
+		ceo = "Larry";
+	}
+	
+	public Emp() { //Constructor get called each time the object is created.
+		eid = 1;
+		salary = 2000;
+	}
+	
 	public void display() {
 		System.out.println("eid :" + eid + "salary: " + salary + "ceo: " + ceo);
 	}
@@ -13,19 +22,26 @@ class Emp {
 
 public class StaticKeyword {
 
+	int i = 0;
 	public static void main(String[] args) {
+		
+		i++; //We cannot access nonstatic variables in a static method.
+		
 		Emp jyoti = new Emp(); //Creating an object by calling the contructor
 		jyoti.eid = 12345;
 		jyoti.salary = 12_000;
-		jyoti.ceo = "Ram";
+		
 		
 		Emp ranjan = new Emp();
 		ranjan.eid = 67891;
 		ranjan.salary = 13_000;
-		ranjan.ceo = "Shyam"; //If the static ceo is changed to Shyam, 
+		Emp.ceo = "Shyam"; //If the static ceo is changed to Shyam, 
 							  //the change is affected all over
 							  //Because now the ceo data is store in the Class Loader Memory
 							 //Earlier it was stored in heap memory.
+		//There is not need to create an object for calling static variable.
+		//Same goes for static methods.
+		//That's why the main() is static
 		System.out.println("Jyoti: ");
 		jyoti.display();
 		
