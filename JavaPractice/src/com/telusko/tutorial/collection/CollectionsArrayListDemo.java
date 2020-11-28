591896c640cbc336129dba839ca24130865227c5
JyotiRSharma <appdeveloperubuntu@gmail.com>
2020-11-29 01:20:13 +0530

    Looking at List interface

--- JavaPractice/src/com/telusko/tutorial/collection/CollectionsArrayListDemo.java
+++ JavaPractice/src/com/telusko/tutorial/collection/CollectionsArrayListDemo.java
@@ -1,15 +1,16 @@
 package com.telusko.tutorial.collection;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 public class CollectionsArrayListDemo {
 	public static void main(String[] args) {
-		Collection values = new ArrayList();
+		List values = new ArrayList();
 		values.add("JYoti");
 		values.add(1);
 		values.add(1.5);
+		values.add(2, "Ranjan"); //We can use List to add values at a specific index
 		
 		Iterator it = values.iterator();
 		
